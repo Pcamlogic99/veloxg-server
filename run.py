abbf57b70e7338767b9f272e56d2b83c0fb1ead1
Pcamlogic99 <kelvinngowo045@gmail.com>
2025-07-21 10:30:51 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-import uvicorn
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to VeloxG Search API"}
