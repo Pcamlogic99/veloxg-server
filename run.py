aebf303218a71e5771275ccc05c9bd629e43573e
Pcamlogic99 <kelvinngowo045@gmail.com>
2025-07-22 13:36:58 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Welcome to VeloxG Search API"}
+
+@app.get("/search")
+def search(query: str = Query(...)):
+    return {"result": f"You searched for: {query}"}
